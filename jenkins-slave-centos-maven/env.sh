c0118efe8e32617db59dc70092cabc0367c786a8
John Inama <john.inama@rackspace.com>
2019-02-14 14:55:05 -0700

    Changed the java home to the proper location

--- jenkins-slave-centos-maven/env.sh
+++ jenkins-slave-centos-maven/env.sh
@@ -4,8 +4,8 @@ MAVEN_HOME=/opt/maven
 M2_HOME=/opt/maven
 M2=$M2_HOME/bin
 MAVEN_OPTS="-Xms256m -Xmx512m"
-JAVA_HOME=/usr/java/jdk1.8.0_102/
-JRE_HOME=/usr/java/jdk1.8.0_102/jre
+JAVA_HOME=/usr/java/latest/
+JRE_HOME=/usr/java/latest/jre
 GROOVY_HOME=/opt/groovy/groovy-2.5.6
 PATH=$ANT_HOME/bin:/opt/maven/bin:/opt/gradle/gradle-5.2.1/bin:$GROOVY_HOME/bin:/usr/local/lib/nodejs/node-v10.15.1-linux-x64/bin:$JAVA_HOME/bin:$PATH
 
