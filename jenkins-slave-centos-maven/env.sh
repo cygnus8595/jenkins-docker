7549d77cfd170d825be5eb01911225745d70a4a1
John Inama <john.inama@rackspace.com>
2019-02-14 14:26:43 -0700

    Put everything in the maven image

--- jenkins-slave-centos-maven/env.sh
+++ jenkins-slave-centos-maven/env.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 ANT_HOME=/opt/ant
-PATH=$ANT_HOME/bin:/opt/maven/bin:$PATH
-export PATH ANT_HOME
+MAVEN_HOME=/opt/maven
+M2_HOME=/opt/maven
+M2=$M2_HOME/bin
+MAVEN_OPTS=-Xms256m -Xmx512m
+JAVE_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.191.b12-1.el7_6.x86_64
+GROOVY_HOME=/opt/groovy
+PATH=$ANT_HOME/bin:/opt/maven/bin:/opt/gradle/gradle-5.2.1/bin:$GROOVY_HOME/bin:/usr/local/lib/nodejs/node-v10.15.1-linux-x64:$PATH
+
+export PATH ANT_HOME MAVEN_HOME M2_HOME MAVEN_OPTS JAVA_HOME GROOVY_HOME
 export CLASSPATH=.
