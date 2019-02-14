577e5457f08b99d7ecb027d71fd25dac96670159
John Inama <john.inama@rackspace.com>
2019-02-14 14:49:13 -0700

    Switched to Oracle JDK and updated env.sh accordingly

--- jenkins-slave-centos-maven/env.sh
+++ jenkins-slave-centos-maven/env.sh
@@ -3,10 +3,11 @@ ANT_HOME=/opt/ant
 MAVEN_HOME=/opt/maven
 M2_HOME=/opt/maven
 M2=$M2_HOME/bin
-MAVEN_OPTS=-Xms256m -Xmx512m
-JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.191.b12-1.el7_6.x86_64
+MAVEN_OPTS="-Xms256m -Xmx512m"
+JAVA_HOME=/usr/java/jdk1.8.0_102/
+JRE_HOME=/usr/java/jdk1.8.0_102/jre
 GROOVY_HOME=/opt/groovy/groovy-2.5.6
-PATH=$ANT_HOME/bin:/opt/maven/bin:/opt/gradle/gradle-5.2.1/bin:$GROOVY_HOME/bin:/usr/local/lib/nodejs/node-v10.15.1-linux-x64/bin:$PATH
+PATH=$ANT_HOME/bin:/opt/maven/bin:/opt/gradle/gradle-5.2.1/bin:$GROOVY_HOME/bin:/usr/local/lib/nodejs/node-v10.15.1-linux-x64/bin:$JAVA_HOME/bin:$PATH
 
 export PATH ANT_HOME MAVEN_HOME M2_HOME MAVEN_OPTS JAVA_HOME GROOVY_HOME
 export CLASSPATH=.
