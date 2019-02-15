c0da871c2fd0e25ccc14dcab94bba52ee1b8cd0a
John Inama <john.inama@rackspace.com>
2019-02-15 13:38:04 -0700

    Fixed path for sonarscanner

--- jenkins-slave-centos-maven/env.sh
+++ jenkins-slave-centos-maven/env.sh
@@ -7,7 +7,7 @@ MAVEN_OPTS="-Xms256m -Xmx512m"
 JAVA_HOME=/usr/java/latest/
 JRE_HOME=/usr/java/latest/jre
 GROOVY_HOME=/opt/groovy/groovy-2.5.6
-PATH=$ANT_HOME/bin:/opt/maven/bin:/opt/gradle/gradle-5.2.1/bin:$GROOVY_HOME/bin:/usr/local/lib/nodejs/node-v10.15.1-linux-x64/bin:/opt/sonarscanner/sonar-scanner-cli-3.3.0.1492-linux/bin:$JAVA_HOME/bin:$PATH
+PATH=$ANT_HOME/bin:/opt/maven/bin:/opt/gradle/gradle-5.2.1/bin:$GROOVY_HOME/bin:/usr/local/lib/nodejs/node-v10.15.1-linux-x64/bin:/opt/sonarscanner/sonar-scanner-3.3.0.1492-linux/bin:$JAVA_HOME/bin:$PATH
 
 export PATH ANT_HOME MAVEN_HOME M2_HOME MAVEN_OPTS JAVA_HOME GROOVY_HOME
 export CLASSPATH=.
